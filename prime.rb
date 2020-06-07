eb662f3bba7a4d70b711773f534810fa44823665
Jack Goode <jack.w.goode@icloud.com>
2020-06-07 22:37:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(number)
-  if number.negative()
+  if number.negative() || number.even?
     return false
   elsif number.to_i == 2 || number.to_i.odd?
     return true
-  else return false
+  
   end
 end    
   
